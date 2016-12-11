dddc94be3f2f1b63321e3b5fed63529e5a0c1269
Lee Adcock <lee@katieandlee.com>
2016-12-11 15:43:28 +0000

    solution 1

--- 2016/advent1/advent1.js
+++ 2016/advent1/advent1.js
@@ -1,24 +1,43 @@
 #!/usr/bin/env node
 
 module.exports = {
-  solve: function(input) {
+  getDistanceFromStart: function(input) {
+    return solve(input).distanceFromStart;
+  },
   
-    var direction = 0, x = 0, y = 0;
-    
-    input.split(', ').forEach(function(step) {
-
-      var turn = step.match(/(L|R)/)[0];
-      var distance = 1*step.match(/[0-9]+/)[0];
-      
-      direction = (direction + (turn==='L') ? 270 : 90) % 360;
-      
-      y += (direction===0) ? distance : 0;
-      y += (direction===180) ? -distance : 0;
-      
-      x += (direction===90) ? distance : 0;
-      x += (direction===270) ? -distance : 0;
-    });
-
-    return Math.abs(x)+Math.abs(y);
+  getFirstLocationVisitedTwice: function(input) {
+    return solve(input).firstVisitedTwice;
   }
 };
+
+var solve = function(input) {
+  var locations = [];
+  var firstVisitedTwice = 0;
+  
+  var direction = 0, x = 0, y = 0;
+
+  input.split(', ').forEach(function(step) {
+
+    var turn = step.substr(0,1)
+    var distance = 1*step.substr(1);
+    
+    direction = (direction + ((turn==='L') ? 270 : 90)) % 360;
+    
+    for(var movement=0; movement<distance; movement++) {
+      
+      y += (direction===0) ? 1 : 0;
+      y += (direction===180) ? -1 : 0;
+      
+      x += (direction===90) ? 1 : 0;
+      x += (direction===270) ? -1 : 0;
+      
+      if(locations.indexOf(x+','+y) != -1) {
+        if(!firstVisitedTwice) firstVisitedTwice = Math.abs(x)+Math.abs(y);
+      } else {
+        locations.push(x+','+y);
+      }
+    }
+  });
+  
+  return {'firstVisitedTwice': firstVisitedTwice, 'distanceFromStart': Math.abs(x)+Math.abs(y)};
+}
