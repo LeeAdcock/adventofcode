f735ee90387069ccf7534b2151a0edd54c928de8
Lee Adcock <lee@katieandlee.com>
2016-12-26 05:09:05 +0000

    solution to problem 8

--- 2016/advent8/advent8Test.js
+++ 2016/advent8/advent8Test.js
@@ -3,6 +3,7 @@
 var advent8 = require('./advent8');
 var assert = require('unit.js').assert;
 
+// Examples
 advent8.init(7, 3);
 advent8.rect(3, 2);
 assert.deepEqual([
@@ -29,80 +30,7 @@ assert.deepEqual([
         [0,1,0,0,0,0,0]
     ], advent8.getScreen());
 
-
-// Test small matrix init
-advent8.init(1, 1);
-assert.deepEqual([[0]], advent8.getScreen());
-
-// Test non-square matrix init
-advent8.init(2, 3);
-assert.deepEqual([
-    [0, 0], 
-    [0, 0], 
-    [0, 0]
-    ], advent8.getScreen());
-
-// Test larger matrix
-advent8.init(5, 5);
-assert.deepEqual(
-    [
-        [0, 0, 0, 0, 0], 
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0]
-    ], advent8.getScreen());
-
-// Test rect
-advent8.init(5, 5);
-advent8.rect(3, 5);
-assert.deepEqual(
-    [
-        [1, 1, 1, 0, 0], 
-        [1, 1, 1, 0, 0],
-        [1, 1, 1, 0, 0],
-        [1, 1, 1, 0, 0],
-        [1, 1, 1, 0, 0]
-    ], advent8.getScreen());
-    
-// Test rotate row
-advent8.init(5, 5);
-advent8.rect(3, 5);
-advent8.rotateRow(0, 1);
-advent8.rotateRow(1, 2);
-advent8.rotateRow(2, 3);
-advent8.rotateRow(3, 4);
-advent8.rotateRow(4, 5);
-assert.deepEqual(
-    [
-        [0, 1, 1, 1, 0], 
-        [0, 0, 1, 1, 1],
-        [1, 0, 0, 1, 1],
-        [1, 1, 0, 0, 1],
-        [1, 1, 1, 0, 0]
-    ], advent8.getScreen());
-
-// Test column rotate
-advent8.init(5, 5);
-advent8.rect(5, 3);
-advent8.rotateColumn(0, 1);
-advent8.rotateColumn(1, 2);
-advent8.rotateColumn(2, 3);
-advent8.rotateColumn(3, 4);
-advent8.rotateColumn(4, 5);
-assert.deepEqual(
-    [ 
-        [ 0, 0, 1, 1, 1 ],
-        [ 1, 0, 0, 1, 1 ],
-        [ 1, 1, 0, 0, 1 ],
-        [ 1, 1, 1, 0, 0 ],
-        [ 0, 1, 1, 1, 0 ] 
-    ], advent8.getScreen());
-    
-assert.equal(15, advent8.getActivatedCount());
-
-
-
+// Large data set
 require('fs').readFile("input.data", 'utf8', function(err, data) {
 
     advent8.init(50, 6);
